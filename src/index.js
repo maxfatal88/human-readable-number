f10c2ed290fd8e33e23a7df68fa1f680b0c20d40
Yavorskiy Maksim <maxfatal@mail.ru>
2023-01-10 20:38:17 +0300

    resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function toReadable (number) {
+
+ module.exports = function toReadable (number) {
   
-}
+    let result = '';
+
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+                'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+                'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy','eighty',
+                'ninety'];
+  
+    let numString = number.toString();
+ 
+    if (number === 0) return 'zero';
+  
+    if (number < 20) {
+      return ones[number];
+    }
+  
+    if (numString.length === 2) {
+      result=tens[numString[0]] + ' ' + ones[numString[1]];
+    }
+  
+    if (numString.length == 3) {
+      if (numString[1] === '0' && numString[2] === '0')
+        result= ones[numString[0]] + ' hundred';
+      else
+         result= ones[numString[0]] + ' hundred ' + toReadable(+(numString[1] + numString[2]));
+    }
+    
+    return result[result.length-1]==' '?result.slice(0,-1):result;
+
+  }
